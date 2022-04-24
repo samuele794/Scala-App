52533a00b5d1fb070edd6e7a27e0ec0d387c5787
Samuele Bruschi <samuele.bruschi@abstract.it>
2022-04-25 00:30:38 +0200

    Add OnBoardingVM on Application

--- androidApp/src/main/java/it/samuele794/scala/android/ScalaApplication.kt
+++ androidApp/src/main/java/it/samuele794/scala/android/ScalaApplication.kt
@@ -6,6 +6,7 @@ import dev.gitlive.firebase.initialize
 import it.samuele794.scala.initKoin
 import it.samuele794.scala.viewmodel.auth.AuthViewModel
 import it.samuele794.scala.viewmodel.auth.NativeAuth
+import it.samuele794.scala.viewmodel.onboarding.OnBoardingViewModel
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.parameter.parametersOf
@@ -24,6 +25,7 @@ class ScalaApplication : Application() {
                 factory { NativeAuth(get(), BuildConfig.GOOGLE_AUTH_APIKEY) }
 
                 viewModel { AuthViewModel(get(), get { parametersOf("AuthViewModel") }) }
+                viewModel { OnBoardingViewModel() }
             }
         )
     }
