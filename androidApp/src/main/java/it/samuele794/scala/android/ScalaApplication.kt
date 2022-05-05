2e28857b1b48e22b4074c42031dfa2a494c7b7c3
Samuele Bruschi <samuele.bruschi@abstract.it>
2022-05-05 21:52:52 +0200

    [Android] Update Koin Injection

--- androidApp/src/main/java/it/samuele794/scala/android/ScalaApplication.kt
+++ androidApp/src/main/java/it/samuele794/scala/android/ScalaApplication.kt
@@ -8,10 +8,13 @@ import it.samuele794.scala.viewmodel.auth.AuthViewModel
 import it.samuele794.scala.viewmodel.auth.NativeAuth
 import it.samuele794.scala.viewmodel.login.LoginViewModel
 import it.samuele794.scala.viewmodel.onboarding.OnBoardingViewModel
+import it.samuele794.scala.viewmodel.onboarding.TrainerLocationSearchViewModel
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.parameter.parametersOf
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
+
 
 class ScalaApplication : Application() {
     override fun onCreate() {
@@ -23,7 +26,9 @@ class ScalaApplication : Application() {
                 androidContext(this@ScalaApplication)
             },
             appModule = module {
-                factory { NativeAuth(get(), BuildConfig.GOOGLE_AUTH_APIKEY) }
+                single(named("GOOGLE_API_KEY")) { BuildConfig.GOOGLE_AUTH_APIKEY }
+
+                factory { NativeAuth(get(), get(named("GOOGLE_API_KEY"))) }
 
                 viewModel { AuthViewModel(get(), get { parametersOf("AuthViewModel") }) }
                 viewModel { parameters ->
@@ -33,7 +38,18 @@ class ScalaApplication : Application() {
                         get()
                     )
                 }
-                viewModel { OnBoardingViewModel(get { parametersOf("OnBoardingViewModel") }) }
+                viewModel {
+                    OnBoardingViewModel(
+                        get { parametersOf("OnBoardingViewModel") }
+                    )
+                }
+
+                viewModel {
+                    TrainerLocationSearchViewModel(
+                        get { parametersOf("TrainerLocationSearchViewModel") },
+                        get()
+                    )
+                }
             }
         )
     }
