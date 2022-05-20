fbd11abef4ba61962790bfa91511a4324c677776
Samuele Bruschi <samuele.bruschi@abstract.it>
2022-05-20 12:15:38 +0200

    Downgraded android build tool

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
-        classpath("com.android.tools.build:gradle:7.1.3")
+        classpath("com.android.tools.build:gradle:7.0.4")
         classpath("dev.icerock.moko:resources-generator:0.19.0")
         classpath("com.google.firebase:firebase-appdistribution-gradle:3.0.1")
         classpath("com.codingfeline.buildkonfig:buildkonfig-gradle-plugin:0.11.0")
